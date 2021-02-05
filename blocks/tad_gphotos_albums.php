c25c559a7bd47e07c8c1cc2f2d724ce56f723112
Tad <tad0616@gmail.com>
2021-02-05 16:32:19 +0800

    避免第一次載入時區塊出現無法取得物件的問題

--- blocks/tad_gphotos_albums.php
+++ blocks/tad_gphotos_albums.php
@@ -1,5 +1,9 @@
 <?php
 use XoopsModules\Tadtools\Utility;
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
+
 /**
  * Tad Gphotos module
  *
--- blocks/tad_gphotos_thumbs.php
+++ blocks/tad_gphotos_thumbs.php
@@ -1,5 +1,8 @@
 <?php
 use XoopsModules\Tadtools\Utility;
+if (!class_exists('XoopsModules\Tadtools\Utility')) {
+    require XOOPS_ROOT_PATH . '/modules/tadtools/preloads/autoloader.php';
+}
 
 /**
  * Tad Gphotos module
